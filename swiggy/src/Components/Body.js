b5636d90fa8a6ae12654f1fe30532a3c96c11b03
KUSHAL BANKHEDE <kushalbankhede1@gmail.com>
2024-07-15 14:38:52 +0530

    CORS error

--- swiggy/src/Components/Body.js
+++ swiggy/src/Components/Body.js
@@ -21,7 +21,7 @@ const Body =()=>{
     } , [])
 
     const fetchData = async()=>{
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.87560&lng=80.91150&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        const data = await fetch("/dapi/restaurants/list/v5?lat=26.87560&lng=80.91150&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
         );
         
         const json = await data.json();
--- swiggy/src/Utils/constant.js
+++ swiggy/src/Utils/constant.js
@@ -2,4 +2,4 @@ export const LOGO_URL = "https://thumbs.dreamstime.com/z/food-delivery-logo-desi
 
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.1766701&lng=78.00807449999999&restaurantId="
+export const MENU_API = "/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.1766701&lng=78.00807449999999&restaurantId="
