a26cd8dd4e6b8bcb8cb0f2d81634f9e68271c87f
KUSHAL BANKHEDE <kushalbankhede1@gmail.com>
2024-07-17 23:37:59 +0530

    test api

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const restaurentRout = require('./routers/restaurentApi');
 const menuRout = require('./routers/menuApi');
 const cors = require('cors');
+const test = require("./controler/test");
 
 const PORT = process.env.PORT || 3000;
 app.use(cors());
@@ -11,7 +12,7 @@ app.use(cors());
 //     res.send("hello world");
 // })
 
-app.use('/api' , restaurentRout ,  menuRout);
+app.use('/api' , restaurentRout ,  menuRout , test);
 
 app.listen(PORT , ()=>{
     console.log(`server is running on port ${PORT}`);
