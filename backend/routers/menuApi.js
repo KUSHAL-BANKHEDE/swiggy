9f6ec8b56c4844afc3ad2a646b006c0b95abbdd2
KUSHAL BANKHEDE <kushalbankhede1@gmail.com>
2024-07-17 14:47:07 +0530

    separate backend

--- backend/routers/menuApi.js
+++
@@ -1,7 +0,0 @@
-const {Router} = require("express");
-const menu = require('../controler/menu')
-const router = Router();
-
-router.get("/restaurant/:resId" , menu);
-
-module.exports = router;
--- backend/routers/restaurentApi.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require("express");
-
-const restaurent = require("../controler/restaurent");
-
-const router = Router();
-
-router.get("/restaurant" , restaurent);
-
-module.exports = router;
