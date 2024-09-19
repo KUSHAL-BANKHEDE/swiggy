b6d10aa046e82c975979054b7f9b75afbf93bcf8
KUSHAL BANKHEDE <kushalbankhede1@gmail.com>
2024-09-19 10:07:17 +0530

    edit Shimmer

--- swiggy/src/Components/shimmers/bodyShimmer.js
+++ swiggy/src/Components/shimmers/bodyShimmer.js
@@ -1,33 +1,21 @@
 const Shimmer=()=>{
     return (
    
-     <div className="shimmer-container">
-
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-        <div className="shimmer-cards"></div>
-
-
-    </div>
-    );
-};
+      
+            <div className="resoCart w-56 min-h-96 bg-gray-200 shadow-lg rounded-lg p-2 m-3 flex flex-col justify-between animate-pulse">
+              <div className="w-full h-42 bg-gray-300 rounded-t-lg"></div>
+              <div className="mt-4 h-6 bg-gray-300 rounded w-3/4"></div>
+              <div className="flex justify-between mt-4">
+                <div className="h-6 bg-gray-300 rounded w-16"></div>
+                <div className="h-6 bg-gray-300 rounded w-16"></div>
+              </div>
+              <div className="mt-2 h-6 bg-gray-300 rounded w-3/5"></div>
+              <div className="mt-2 h-6 bg-gray-300 rounded w-1/2"></div>
+            </div>
+          );
+        };
+        
+    
 
 export default Shimmer;
 
